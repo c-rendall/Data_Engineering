af5adb9319ed0720555a45ccfb8c90add73b3610
Chase Rendall <chaserendall@Chases-MacBook-Air.local>
2024-04-09 13:05:28 -0500

    updated fields in sql table

--- coincap.py
+++ coincap.py
@@ -14,35 +14,51 @@ def fetch_coin_data() -> None:
             data = response.json()
             coin_data = data['data']
 
-            # Save data to csv file 
-            df = pd.DataFrame(coin_data)
-            csv_filename = f"cryptocurrency_data_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-            df.to_csv(csv_filename, index=False)
-
             # Create SQLite table 
             conn = sqlite3.connect('cryptocurrency_data.db')
             cur = conn.cursor()
             cur.execute("""
             CREATE TABLE IF NOT EXISTS cryptocurrencies(
-                id INTEGER PRIMARY KEY, 
-                name TEXT,
+                id TEXT,
+                rank INT,
                 symbol TEXT,
+                name TEXT,
+                supply REAL,
+                max_supply REAL,
+                market_cap_usd REAL,
+                volume_usd_24hr REAL,
                 price_usd REAL, 
-                fetch_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                change_percent_24hr REAL,
+                vwap_24hr REAL,
+                explorer TEXT,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
             """)
 
+            # Add timestamp to coin data 
+            timestamp = datetime.datetime.now()
+
             # Save data into cryptocurrencies table
             for coin in coin_data: 
+                coin['timestamp'] = timestamp
                 cur.execute("""
-                INSERT INTO cryptocurrencies (name, symbol, price_usd)
-                VALUES (?, ?, ?)
-                """, (coin['name'], coin['symbol'], coin['priceUsd'])
+                INSERT INTO cryptocurrencies (id, rank, symbol, name, supply, max_supply, market_cap_usd, volume_usd_24hr,
+                                              price_usd, change_percent_24hr, vwap_24hr, explorer, timestamp)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """, (coin['id'], coin['rank'], coin['symbol'], coin['name'], coin['supply'], coin['maxSupply'],
+                      coin['marketCapUsd'], coin['volumeUsd24Hr'], coin['priceUsd'], coin['changePercent24Hr'], coin['vwap24Hr'],
+                      coin['explorer'], coin['timestamp'])
                 )
 
             conn.commit()
             cur.close()
             conn.close()
+
+            # Save data to csv file 
+            df = pd.DataFrame(coin_data)
+            csv_filename = f"cryptocurrency_data_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+            df.to_csv(csv_filename, index=False)
+
             print(f"{len(coin_data)} records inserted successfully into SQLite at {datetime.datetime.now()}!")
 
         else:
